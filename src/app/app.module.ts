bb833f8a31a8b79c6ce349a0406542324aac5b27
Axel-Ly <axelly.ly@gmail.com>
2021-03-06 18:03:59 +0100

    Ajout 1er fichier

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FonctionnementComponent } from './View/fonctionnement/fonctionnement.component';
+import { AProposComponent } from './View/a-propos/a-propos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FonctionnementComponent,
+    AProposComponent
   ],
   imports: [
     BrowserModule,
