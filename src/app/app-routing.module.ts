709b57227d03a8e02cb60697e9adcbbeea346616
MathieuCarvajal <mathieu.carvajal@yahoo.fr>
2021-03-07 18:39:07 +0100

    Mise en place du routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { AProposComponent } from './View/a-propos/a-propos.component';
+import { FonctionnementComponent } from './View/fonctionnement/fonctionnement.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home', component:AppComponent},
+  {path: 'about', component:AProposComponent},
+  {path: 'fonctionnement', component:FonctionnementComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponents = [AProposComponent, FonctionnementComponent]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FonctionnementComponent } from './View/fonctionnement/fonctionnement.component';
-import { AProposComponent } from './View/a-propos/a-propos.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FonctionnementComponent,
-    AProposComponent
+    routingComponents
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
